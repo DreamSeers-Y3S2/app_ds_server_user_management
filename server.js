ac151ecb65b1a96b317d9b1f2ebf519363d6d759
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2023-04-18 23:00:50 +0530

    updated server file for user

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const connectDB = require("./config/db");
 const adminRoutes = require("./routes/adminRoutes");
@@ -12,18 +11,18 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 connectDB();
 app.use(express.json());
-app.use("*", cors());
+// app.use("*", cors());
 
 app.get("/", (req, res) => {
 	res.send("User API is Running");
 });
 
-app.use("/user/admin", adminRoutes);
-app.use("/user/customer", customerRoutes);
-app.use("/user/vendor", vendorRoutes);
+app.use("/admin", adminRoutes);
+app.use("/customer", customerRoutes);
+app.use("/vendor", vendorRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, console.log(`Server Started on port ${PORT}..`));
